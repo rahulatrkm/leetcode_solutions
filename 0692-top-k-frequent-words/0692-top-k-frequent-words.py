51afd67bcaf9df307eff3e12638571b2aff56074
Rahul <rahulatrkm@gmail.com>
2022-10-19 07:06:03 +0530

    Time: 109 ms (55.92%), Space: 14 MB (27.71%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,34 +1,3 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        def calc_hash(word, l):
-            curr = ["26"]*l
-            # print(curr)
-            for i,char in enumerate(word):
-                curr[i] = str(ord('z')-ord(char))
-                if len(curr[i]) == 1:
-                    curr[i] = "0" + curr[i]
-            return "".join(curr)
-        
-        freq = collections.defaultdict(int)
-        l = 0
-        for word in words:
-            freq[word] += 1
-            l = max(len(word), l)
-        
-        topk = []
-        for word in freq:
-            hsh = calc_hash(word, l)
-            if len(topk) < k:
-                heapq.heappush(topk, (freq[word], hsh, word))
-            else:
-                if (topk[0][0], topk[0][1]) < (freq[word], hsh):
-                    heapq.heappop(topk)
-                    heapq.heappush(topk, (freq[word],hsh, word))
-                
-            # print(topk)
-        ans = []
-        topk.sort(key = lambda x: (-x[0], x[2]))
-        for word in topk:
-            ans.append(word[2])
-        return ans
-            
+        return [word[0] for word in sorted(Counter(words).items(), key = lambda x: (-x[1], x[0]))[:k]]

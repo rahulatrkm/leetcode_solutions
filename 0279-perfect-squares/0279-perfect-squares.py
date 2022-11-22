a41b353eb617fe9190257a039ec5e25748423078
Rahul <rahulatrkm@gmail.com>
2022-11-22 13:50:32 +0530

    Time: 331 ms (79.01%), Space: 14.1 MB (54.15%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,26 @@
+import math
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        q1 = [0]
+        q2 = []
+        level = 0
+        visited = [False] * (n+1)
+        while True:
+            level += 1
+            for v in q1:
+                i = 0
+                while True:
+                    i += 1
+                    t = v + i * i
+                    if t == n: return level
+                    if t > n: break
+                    if visited[t]: continue
+                    q2.append(t)
+                    visited[t] = True
+            q1 = q2
+            q2 = []
+            # print(q1)
+                
+        return 0
+            
